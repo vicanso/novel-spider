b5a0abb410bf616b52d88f85114dbf5c32d25ab0
vicanso <vicansocanbico@gmail.com>
2016-05-26 22:52:23 +0800

    commit

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,24 @@
 'use strict';
 const Qidian = require('../lib/qidian');
-Qidian.search('九鼎记').then(id => {
-  console.dir(id);
-}).catch(err => {
-  console.error(err);
-});
+const Wx23 = require('../lib/wx23');
+// Qidian.search('九鼎记').then(id => {
+//   console.dir(id);
+// }).catch(err => {
+//   console.error(err);
+// });
+
+const get = (id) => {
+  const wx = new Wx23(id);
+  wx.getInfo().then(infos => {
+    if (infos) {
+      infos.id = '' + id;
+      console.info(infos);
+    }
+    get(++id);
+  }).catch(err => {
+    console.error(`fail to get:${id}`);
+    get(++id);
+  });
+}
+
+get(1);
