40be541c56f2d1656d8adc956ce9bbce192b0afb
vicanso <vicansocanbico@gmail.com>
2016-05-28 09:29:20 +0800

    commit

--- demo/index.js
+++ demo/index.js
@@ -1,24 +1,11 @@
 'use strict';
 const Qidian = require('../lib/qidian');
-const Wx23 = require('../lib/wx23');
+Qidian.getAllInfosByName('九鼎记').then(data => {
+  console.dir(data);
+});
 // Qidian.search('九鼎记').then(id => {
-//   console.dir(id);
+//   return new Qidian(id);
 // }).catch(err => {
 //   console.error(err);
 // });
 
-const get = (id) => {
-  const wx = new Wx23(id);
-  wx.getInfo().then(infos => {
-    if (infos) {
-      infos.id = '' + id;
-      console.info(infos);
-    }
-    get(++id);
-  }).catch(err => {
-    console.error(`fail to get:${id}`);
-    get(++id);
-  });
-}
-
-get(1);
---
+++ tasks/novel.js
@@ -0,0 +1,60 @@
+'use strict';
+const Models = require('../models');
+const Wx23 = require('../lib/wx23');
+const Qidian = require('../lib/qidian');
+
+const getMaxId = (name) => {
+  return Models.get(name).findOne().sort({_id: 'desc'}).then(doc => {
+    if (!doc) {
+      return 0;
+    } else {
+      return doc.toJSON().id;
+    }
+  });
+}
+
+const sync = (name, start, end) => {
+  let Novel = Qidian;
+  if (name === 'Wx23') {
+    Novel = Wx23;
+  }
+  const Model = Models.get(name);
+  const get = (id) => {
+    if (id > end) {
+      return;
+    }
+    const novel = new Novel(id);
+    novel.getInfo().then(infos => {
+      if (!infos) {
+        return null;
+      }
+      infos.id = id;
+      const model = new Model(infos);
+      console.info(`get ${name} ${id} novel info success`);
+      return model.save();
+    }).then(data => {
+      get(++id);
+    }).catch(err => {
+      console.error(`get ${name} ${id} novel info fail,%s`, err);
+      get(++id);
+    });
+  };
+  get(start);
+};
+
+
+exports.sync = (name, max) => {
+  getMaxId(name).then(id => {
+    sync(name, ++id, max);
+  }).catch(err => {
+    console.error(err);
+  });
+};
+
+exports.syncWx23 = (max) => {
+  exports.sync('Wx23', max);
+};
+
+exports.syncQidian = (max) => {
+  exports.sync('Qidian', max);
+};
