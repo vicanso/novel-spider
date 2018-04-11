b3868e71549b384ce84a77a91c40c2025cff0950
vicanso <vicansocanbico@gmail.com>
2018-04-11 21:18:46 +0800

    feat: 增加`xbiquge`来源处理

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,8 @@
 const BiQuGe = require('../lib/biquge');
+const XBiQuGe = require('../lib/xbiquge');
 
-const novel = new BiQuGe(18949);
+// const novel = new BiQuGe(18949);
+const novel = new XBiQuGe(66066);
 
 novel.getInfos().then(console.dir);
 novel.getChapters().then(console.dir);
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 exports.BiQuGe = require('./lib/biquge');
+exports.XBiQuGe = require('./lib/xbiquge');
 exports.request = require('./lib/request');
